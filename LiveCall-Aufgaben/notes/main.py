3cd10ab45eddec2ee1eb70b39f3860997084e6ef
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-09-01 11:55:01 +0200

    added delete_note

--- LiveCall-Aufgaben/notes/main.py
+++ LiveCall-Aufgaben/notes/main.py
@@ -7,16 +7,22 @@ def show_notes():
     for note in notes:
         print(f"Title: {note['title']}, Text: {note['text']}")
 
-def add_note():
-    pass 
+def add_note(title, text):
+    notes.append({"title": title, "text": text})
 
-def delete_note():
-    pass 
-
+def delete_note(title):
+    for note in notes:
+        if note["title"] == title:
+            notes.remove(note)
+            break
+    
 def update_note():
     pass
 
-# add_note()
 # delete_note()
 # update_note()
+add_note("Hausarbeit", "Fußboden wischen")
+show_notes()
+delete_note("Hausarbeit")
+print("----")
 show_notes()
