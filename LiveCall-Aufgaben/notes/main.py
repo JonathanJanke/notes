01f4c9ab98ea6697d88412597c3e6d0fc7e57f39
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-09-02 12:43:59 +0200

    Added Excercise

--- LiveCall-Aufgaben/notes/main.py
+++ LiveCall-Aufgaben/notes/main.py
@@ -1,3 +1,5 @@
+# Hausaufgabe LiveCall
+
 notes = [
     {"title": "Einkauf", "text": "Milch, Brot, Eier"},
     {"title": "Arbeit", "text": "Backendcall um 11"},
@@ -31,3 +33,22 @@ show_notes()
 print("----")
 update_note("Arbeit", "Arbeit2", "Backendcall um 13 Uhr")
 show_notes()
+
+# ----
+# Übung Logische Operatoren
+# 1. Welche Wahrheitswerte kommen bei den folgenden Ausdrücken heraus? 
+#  True and False and True or False - 1 && 0 && 1 // 0 Immer False
+#  not False or not True -> Immer True
+#  True and (False or not False) -> Immer true 
+#  not (not False ^ True or not False) -> False
+#  True and False ^ True and False -> False
+
+# 2. Worin besteht der Unterschied zwischen den Operatoren ^ und or?
+# ^ist ein X-or, welches besagt entweder oder. Es können aber nicht beide Bedingungen gleichzeitig true oder false sein.
+
+# 3. Welche Wahrheitswerte kommen als Ergebnis heraus?
+#  2 < 3 and not 2 > 5 -> True
+#  not True ^ False or 3 == 2 + 1 -> True
+#  not not not 2 % 5 == 7 % 5 -> False
+#  True and False ^ True and False -> False
+#  True ^ False ^ 0 ^ 1 ^ (2 > 3) -> False
